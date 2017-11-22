ffb521b66293e8e0ee6f75116de5d49b6b4c73cb
wangyan-9067 <wangyan9067@gmail.com>
2017-11-22 09:18:35 +0800

    add toggleToDo feature

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { toggleTodo } from '../actions';
 
-let ToDoList = ({todos, dispatch}) => (
-    <div>
-        ToDoList
-        <ul>
-            {todos.map((todo, index) => (
-                <li key = {index}>{todo.text}</li>
-            ))}
-        </ul>
-    </div>
-);
+let ToDoList = ({todos, dispatch}) => {
+    return (
+        <div>
+            ToDoList
+            <ul>
+                {
+                    todos.map((todo) => {
+                        let onClick = () => {
+                            dispatch(toggleTodo(todo.id));
+                        };
+
+                        return (
+                            <li
+                                key = {todo.id}
+                                onClick = {onClick}
+                                style = {{
+                                    textDecoration: todo.completed ? 'line-through' : 'none'
+                                }}>
+                                {todo.text}
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    )
+};
 
 let mapStateToProps = (state) => {
     return {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,21 @@ const todos = (state = [], action) => {
                 ...state,
                 {
                     id: action.id,
-                    text: action.text
+                    text: action.text,
+                    completed: false
                 }
             ];
+
+        case "TOGGLE_TODO":
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return Object.assign({}, {...todo}, {
+                        completed: !todo.completed
+                    });
+                }
+
+                return todo;
+            });
         default:
             return state;
     }
